106e7140cba97db091769ef0b22c1a308c294c4f
Rik Vanmechelen <rvanmech@pratt.edu>
2026-02-24 20:03:37 -0500

    author edit is now a thing!

--- models/author.js
+++ models/author.js
@@ -6,6 +6,21 @@ const authors = [
 ]
 
 exports.all = authors;
+exports.upsert = (author) => {
+  if (author.id) {
+    exports.update(author);
+  } else {
+    exports.add(author);
+  }
+}
+
 exports.add = (author) => {
   authors.push(author);
 };
+exports.update = (author) => {
+  author.id = parseInt(author.id);
+  authors[author.id] = author;
+}
+exports.get = (idx) => {
+  return authors[idx];
+}
--- routes/authors.js
+++ routes/authors.js
@@ -12,10 +12,16 @@ router.get('/form', function(req, res, next) {
   res.render('authors/form', { title: 'BookedIn || Authors' });
 });
 
-router.post('/create', function(req, res, next) {
+router.post('/upsert', function(req, res, next) {
   console.log(JSON.stringify(req.body));
-  Author.add(req.body);
+  Author.upsert(req.body);
   res.redirect(303, "/authors");
+});
+
+router.get('/edit', function(req, res, next) {
+  let authorIdx = req.query.id
+  let author = Author.get(authorIdx);
+  res.render('authors/form', { title: 'BookedIn || Authors', author: author, authorIdx: authorIdx });
 });
 
 module.exports = router;
