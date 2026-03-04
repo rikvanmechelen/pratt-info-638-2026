fa7062fbda6d9a08979bd598e94467fb03840ae5
Rik Vanmechelen <rvanmech@pratt.edu>
2026-03-03 19:59:29 -0500

    book has authors!

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const Book = require('../models/book');
+const Author = require('../models/author');
 
 const router = express.Router();
 
@@ -10,10 +11,10 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/form', async (req, res, next) => {
-  res.render('books/form', { title: 'BookedIn || Books' });
+  res.render('books/form', { title: 'BookedIn || Books', authors: Author.all  });
 });
 
-router.post('/create', async (req, res, next) => {
+router.post('/upsert', async (req, res, next) => {
   console.log('body: ' + JSON.stringify(req.body))
   Book.upsert(req.body);
   res.redirect(303, '/books')
@@ -22,7 +23,18 @@ router.post('/create', async (req, res, next) => {
 router.get('/edit', async (req, res, next) => {
   let bookIdx = req.query.id;
   let book = Book.get(bookIdx);
-  res.render('books/form', { title: 'BookedIn || Books', book: book, bookIdx: bookIdx });
+  res.render('books/form', { title: 'BookedIn || Books', book: book, bookIdx: bookIdx, authors: Author.all  });
+});
+
+router.get('/show/:id', async (req, res, next) => {
+  let bookIdx = req.params.id;
+  let book = Book.get(bookIdx);
+  let author = Author.get(book.authorId);
+  let authors = []
+  if (book.authorIds) {
+    authors = book.authorIds.map(Author.get)
+  }
+  res.render('books/show', { title: 'BookedIn || Book', book: book, bookIdx: bookIdx, authors: authors });
 });
 
 module.exports = router;
